3d5d83ca0070ff45cf84d962ee3b9fdca8ff577b
Toma Chen <tchen346@wisc.edu>
2026-03-05 15:34:33 -0800

    prevent sql injection

--- webapp/public/login.php
+++ webapp/public/login.php
@@ -26,8 +26,11 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
     $username = $_POST['username'];
     $password = $_POST['password'];
 
-    $sql = "SELECT * FROM users WHERE username = '$username' AND password = '$password' AND approved = 1";
-    $result = $conn->query($sql);
+    $sql = "SELECT * FROM users WHERE username = ? AND password = ? AND approved = 1";
+    $stmt = $conn->prepare($sql);
+    $stmt->bind_param("ss", $username, $password);
+    $stmt->execute();
+    $result = $stmt->get_result();
 
     if($result->num_rows > 0) {
        
